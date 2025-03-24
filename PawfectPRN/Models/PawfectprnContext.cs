95b7e60e06360ab6b4b54589460b5ffbfbade639
NguyenNghj <nghintce180897@fpt.edu.vn>
2025-03-24 12:17:22 +0700

    Hoan thanh Customer Booking

--- PawfectPRN/Models/PawfectprnContext.cs
+++ PawfectPRN/Models/PawfectprnContext.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace PawfectPRN.Models;
 
@@ -29,9 +31,20 @@ public partial class PawfectPrnContext : DbContext
 
     public virtual DbSet<Product> Products { get; set; }
 
+    private string GetConnectionString()
+    {
+        IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .Build();
+        var connectionstring = configuration["ConnectionStrings:DefaultConnection"];
+        return connectionstring;
+    }
+
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=NITRO5\\SQLEXPRESS;Initial Catalog=PawfectPRN;User ID=sa;Password=123456789;Trust Server Certificate=True");
+        => optionsBuilder.UseSqlServer(GetConnectionString());
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- PawfectPRN/ViewModels/CustomerBookingViewModel.cs
+++ PawfectPRN/ViewModels/CustomerBookingViewModel.cs
@@ -2,17 +2,19 @@
 using FirstCode.ViewModels;
 using Newtonsoft.Json;
 using PawfectPRN.Models;
+using System;
 using System.Collections.ObjectModel;
-using System.Windows.Input;
+using System.Linq;
 using System.Windows;
+using System.Windows.Input;
 using Microsoft.EntityFrameworkCore;
+using PawfectPRN.Validation;
 
 public class CustomerBookingViewModel : BaseViewModel
 {
     public Account Account { get; set; }
     public ObservableCollection<PethotelBooking> AllBookings { get; set; }
     public ObservableCollection<PethotelBooking> PethotelBookings { get; set; }
-
     public ObservableCollection<PetHotel> Pethotels { get; set; }
 
     public ICommand AddCommand { get; set; }
@@ -21,15 +23,68 @@ public class CustomerBookingViewModel : BaseViewModel
     public ICommand SearchCommand { get; set; }
     public ICommand ResetCommand { get; set; }
 
+    private PethotelBooking _textboxItem;
+    public PethotelBooking TextboxItem
+    {
+        get => _textboxItem;
+        set
+        {
+            _textboxItem = value;
+            OnPropertyChanged(nameof(TextboxItem));
+            UpdatePrice();
+        }
+    }
+
+    private PethotelBooking _selectedItem;
+    public PethotelBooking SelectedItem
+    {
+        get => _selectedItem;
+        set
+        {
+            _selectedItem = value;
+            OnPropertyChanged(nameof(SelectedItem));
+            if (_selectedItem != null)
+            {
+                var settings = new JsonSerializerSettings
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                };
+                TextboxItem = JsonConvert.DeserializeObject<PethotelBooking>(
+                    JsonConvert.SerializeObject(_selectedItem, settings));
+                OnPropertyChanged(nameof(TextboxItem));
+            }
+        }
+    }
+
+    private string _searchText;
+    public string SearchText
+    {
+        get => _searchText;
+        set { _searchText = value; OnPropertyChanged(nameof(SearchText)); }
+    }
+
     public CustomerBookingViewModel(Account account)
     {
         Account = account;
+        AllBookings = new ObservableCollection<PethotelBooking>();
+        PethotelBookings = new ObservableCollection<PethotelBooking>();
+        Pethotels = new ObservableCollection<PetHotel>();
+
+        TextboxItem = new PethotelBooking
+        {
+            AccountId = account.AccountId,
+            BookingDate = DateTime.Now,
+            CheckoutDate = DateTime.Now.AddDays(1)
+        };
+
         AddCommand = new RelayCommand(Add);
         UpdateCommand = new RelayCommand(Update);
         DeleteCommand = new RelayCommand(Delete);
         SearchCommand = new RelayCommand(Search);
         ResetCommand = new RelayCommand(Reset);
-        TextboxItem = new PethotelBooking();
+
+        LoadBookings();
+        LoadPethotels();
     }
 
     private void LoadBookings()
@@ -37,100 +92,142 @@ public class CustomerBookingViewModel : BaseViewModel
         using (var context = new PawfectPrnContext())
         {
             var bookingsFromDb = context.PethotelBookings
-                                         .Include(b => b.Pethotel)  // Ensure Pethotel is loaded
-                                         .Where(b => b.AccountId == Account.AccountId)
-                                         .ToList();
-            AllBookings = new ObservableCollection<PethotelBooking>(bookingsFromDb);
-            PethotelBookings = new ObservableCollection<PethotelBooking>(AllBookings);
+                .Include(b => b.Pethotel)
+                .Where(b => b.AccountId == Account.AccountId)
+                .ToList();
+            AllBookings.Clear();
+            PethotelBookings.Clear();
+            foreach (var booking in bookingsFromDb)
+            {
+                AllBookings.Add(booking);
+                PethotelBookings.Add(booking);
+            }
             OnPropertyChanged(nameof(PethotelBookings));
         }
     }
 
-
-    private PethotelBooking _textboxItem;
-    public PethotelBooking TextboxItem
+    private void LoadPethotels()
     {
-        get { return _textboxItem; }
-        set { _textboxItem = value; OnPropertyChanged(nameof(TextboxItem)); }
+        using (var context = new PawfectPrnContext())
+        {
+            var pethotelsFromDb = context.PetHotels.ToList();
+            Pethotels.Clear();
+            foreach (var pethotel in pethotelsFromDb)
+            {
+                Pethotels.Add(pethotel);
+            }
+            OnPropertyChanged(nameof(Pethotels));
+        }
     }
 
-    private PethotelBooking _selectedItem;
-    public PethotelBooking SelectedItem
+    private void UpdatePrice()
     {
-        get { return _selectedItem; }
-        set
+        if (TextboxItem == null || TextboxItem.PethotelId == 0 || TextboxItem.BookingDate == null || TextboxItem.CheckoutDate == null)
+            return;
+
+        using (var context = new PawfectPrnContext())
         {
-            _selectedItem = value;
-            OnPropertyChanged(nameof(SelectedItem));
-            if (_selectedItem != null)
+            var petHotel = context.PetHotels.Find(TextboxItem.PethotelId);
+            if (petHotel != null)
             {
-                TextboxItem = JsonConvert.DeserializeObject<PethotelBooking>(JsonConvert.SerializeObject(_selectedItem));
-                OnPropertyChanged(nameof(TextboxItem));
+                DateTime bookingDate = TextboxItem.BookingDate.Value;
+                DateTime checkoutDate = TextboxItem.CheckoutDate.Value;
+                if (checkoutDate > bookingDate)
+                {
+                    TimeSpan stayDuration = checkoutDate - bookingDate;
+                    int numberOfDays = stayDuration.Days > 0 ? stayDuration.Days : 1;
+                    TextboxItem.Price = petHotel.Price * numberOfDays;
+                    OnPropertyChanged(nameof(TextboxItem));
+                }
             }
         }
     }
 
-    //private void LoadBookings()
-    //{
-    //    using (var context = new PawfectPrnContext())
-    //    {
-    //        var bookingsFromDb = context.PethotelBookings
-    //                                     .Where(b => b.AccountId == Account.AccountId)
-    //                                     .ToList();
-    //        AllBookings = new ObservableCollection<PethotelBooking>(bookingsFromDb);
-    //        PethotelBookings = new ObservableCollection<PethotelBooking>(AllBookings);
-    //        OnPropertyChanged(nameof(PethotelBookings));
-    //    }
-    //}
-
     private void Add(object obj)
     {
-        if (TextboxItem.PethotelId == 0 || TextboxItem.CheckoutDate == default)
-        {
-            MessageBox.Show("Please enter complete booking information!", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
-            return;
-        }
         using (var context = new PawfectPrnContext())
         {
+            var validator = new CustomerBookingValidator();
+            var petHotel = context.PetHotels.Find(TextboxItem.PethotelId);
+
+            var (isValid, errorMessage) = validator.ValidateAdd(TextboxItem, petHotel);
+            if (!isValid)
+            {
+                MessageBox.Show(errorMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            DateTime bookingDate = TextboxItem.BookingDate ?? DateTime.Now;
+            TimeSpan stayDuration = TextboxItem.CheckoutDate.Value - bookingDate;
+            int numberOfDays = stayDuration.Days > 0 ? stayDuration.Days : 1;
+            decimal calculatedPrice = petHotel.Price * numberOfDays;
+
             var newBooking = new PethotelBooking
             {
-                AccountId = TextboxItem.AccountId,
+                AccountId = Account.AccountId,
                 PethotelId = TextboxItem.PethotelId,
-                BookingDate = DateTime.Now,
-                CheckoutDate = TextboxItem.CheckoutDate,
+                BookingDate = bookingDate,
+                CheckoutDate = TextboxItem.CheckoutDate.Value,
                 ServiceDetails = TextboxItem.ServiceDetails,
-                Status = "Pending"
+                Status = "Pending",
+                Price = calculatedPrice
             };
 
             context.PethotelBookings.Add(newBooking);
             context.SaveChanges();
-            context.Entry(newBooking).Reload();
+            context.Entry(newBooking).Reference(b => b.Pethotel).Load();
 
             PethotelBookings.Add(newBooking);
             AllBookings.Add(newBooking);
+            TextboxItem = new PethotelBooking { AccountId = Account.AccountId, BookingDate = DateTime.Now, CheckoutDate = DateTime.Now.AddDays(1) };
             OnPropertyChanged(nameof(PethotelBookings));
+            OnPropertyChanged(nameof(TextboxItem));
+            MessageBox.Show("Booking created successfully!", "Success",
+                MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 
     private void Update(object obj)
     {
-        if (SelectedItem == null) return;
         using (var context = new PawfectPrnContext())
         {
+            var validator = new CustomerBookingValidator();
+            var petHotel = context.PetHotels.Find(TextboxItem.PethotelId);
+
+            var (isValid, errorMessage) = validator.ValidateUpdate(SelectedItem, TextboxItem, petHotel);
+            if (!isValid)
+            {
+                MessageBox.Show(errorMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
             var bookingToUpdate = context.PethotelBookings.Find(SelectedItem.BookingId);
             if (bookingToUpdate != null)
             {
-                bookingToUpdate.CheckoutDate = TextboxItem.CheckoutDate;
+                DateTime bookingDate = TextboxItem.BookingDate ?? DateTime.Now;
+                TimeSpan stayDuration = TextboxItem.CheckoutDate.Value - bookingDate;
+                int numberOfDays = stayDuration.Days > 0 ? stayDuration.Days : 1;
+                decimal calculatedPrice = petHotel.Price * numberOfDays;
+
+                bookingToUpdate.PethotelId = TextboxItem.PethotelId;
+                bookingToUpdate.BookingDate = bookingDate;
+                bookingToUpdate.CheckoutDate = TextboxItem.CheckoutDate.Value;
                 bookingToUpdate.ServiceDetails = TextboxItem.ServiceDetails;
-                bookingToUpdate.Status = TextboxItem.Status;
+                bookingToUpdate.Status = TextboxItem.Status ?? "Pending";
+                bookingToUpdate.Price = calculatedPrice;
+
                 context.SaveChanges();
-                var index = PethotelBookings.IndexOf(SelectedItem);
+                context.Entry(bookingToUpdate).Reference(b => b.Pethotel).Load();
+
+                int index = PethotelBookings.IndexOf(SelectedItem);
                 if (index != -1)
                 {
                     PethotelBookings[index] = bookingToUpdate;
                     AllBookings[index] = bookingToUpdate;
                 }
                 OnPropertyChanged(nameof(PethotelBookings));
+                MessageBox.Show("Booking updated successfully!", "Success",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
             }
         }
     }
@@ -138,6 +235,7 @@ public class CustomerBookingViewModel : BaseViewModel
     private void Delete(object obj)
     {
         if (SelectedItem == null) return;
+
         using (var context = new PawfectPrnContext())
         {
             var bookingToDelete = context.PethotelBookings.Find(SelectedItem.BookingId);
@@ -148,17 +246,11 @@ public class CustomerBookingViewModel : BaseViewModel
                 AllBookings.Remove(SelectedItem);
                 PethotelBookings.Remove(SelectedItem);
                 SelectedItem = null;
-                TextboxItem = new PethotelBooking();
+                TextboxItem = new PethotelBooking { AccountId = Account.AccountId, BookingDate = DateTime.Now, CheckoutDate = DateTime.Now.AddDays(1) };
                 OnPropertyChanged(nameof(PethotelBookings));
+                OnPropertyChanged(nameof(TextboxItem));
             }
         }
-    }
-
-    private string _searchText;
-    public string SearchText
-    {
-        get => _searchText;
-        set { _searchText = value; OnPropertyChanged(nameof(SearchText)); }
     }
 
     private void Search(object obj)
@@ -173,7 +265,11 @@ public class CustomerBookingViewModel : BaseViewModel
         }
         else
         {
-            var filteredBookings = AllBookings.Where(b => b.ServiceDetails.Contains(SearchText, StringComparison.OrdinalIgnoreCase)).ToList();
+            var filteredBookings = AllBookings.Where(b =>
+                (b.ServiceDetails?.Contains(SearchText, StringComparison.OrdinalIgnoreCase) ?? false) ||
+                (b.Pethotel?.PethotelName?.Contains(SearchText, StringComparison.OrdinalIgnoreCase) ?? false)
+            ).ToList();
+
             PethotelBookings.Clear();
             foreach (var booking in filteredBookings)
             {
@@ -186,8 +282,9 @@ public class CustomerBookingViewModel : BaseViewModel
     private void Reset(object obj)
     {
         SelectedItem = null;
-        TextboxItem = new PethotelBooking();
+        TextboxItem = new PethotelBooking { AccountId = Account.AccountId, BookingDate = DateTime.Now, CheckoutDate = DateTime.Now.AddDays(1) };
         SearchText = string.Empty;
         Search(null);
+        OnPropertyChanged(nameof(TextboxItem));
     }
 }
