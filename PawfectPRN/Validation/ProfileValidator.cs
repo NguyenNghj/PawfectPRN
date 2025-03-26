f281c58c6b3255fb280e05bb754f71fa6f4e7bed
NguyenNghj <nghintce180897@fpt.edu.vn>
2025-03-26 10:13:00 +0700

    Chinh sua validator

---
+++ PawfectPRN/Validation/ProfileValidator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawfectPRN.Validation
+{
+    internal class ProfileValidator
+    {
+        public static bool ValidatePasswordChange(string oldPassword, string newPassword, string confirmNewPassword, string actualHashedPassword, Func<string, string> hashFunction, out string errorMessage)
+        {
+            errorMessage = string.Empty;
+
+            if (string.IsNullOrWhiteSpace(oldPassword) || string.IsNullOrWhiteSpace(newPassword) || string.IsNullOrWhiteSpace(confirmNewPassword))
+            {
+                errorMessage = "Please fill in all the required fields!";
+                return false;
+            }
+
+            if (newPassword != confirmNewPassword)
+            {
+                errorMessage = "New password and confirmation do not match!";
+                return false;
+            }
+
+            string hashedOldPassword = hashFunction(oldPassword);
+            if (hashedOldPassword != actualHashedPassword)
+            {
+                errorMessage = "Incorrect old password!";
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
